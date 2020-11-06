936d85568d0bf4a0d6b6f0f308c1cef4f6f76f70
sebastianrodo <sebastian.rodriguez@koombea.com>
2020-11-06 12:10:26 -0500

    Update Fibonacci Sequence exercise

--- ruby/Fibonacci.rb
+++ ruby/Fibonacci.rb
@@ -1,4 +1,35 @@
 
+  def fibonacci2
+    MAX_VALUE = 4000000
+
+    (1..MAX_VALUE).each_with_object([1,2]) do |num, acum|
+      return acum if acum[acum.size - 1] + acum[acum.size - 2] > MAX_VALUE 
+
+      acum << acum[acum.size - 1] + acum[acum.size - 2]
+    end
+  end
+
+  puts fibonacci2
+
+=begin
+  puts "The Fibonacci sequence is : "
+  initial_variables = [0, 1]
+   def(initial_variables)
+       
+   end
+  
+  (35).times do
+    x = arry[0]+arry[1]
+    arry[0]=arry[1]
+    break if x>=4000000
+    arry[1]=x
+      puts arry[1]
+  end
+=end
+
+=begin
+
+
 def fibonacci(max_value)
     puts "The Fibonacci sequence is : "
   
@@ -16,22 +47,7 @@ def fibonacci(max_value)
   end
    
   fibonacci(4000000) {|x| puts x }
-  
-  =begin
-  
-  puts "The Fibonacci sequence is : "
-  initial_variables = [0, 1]
-   def(initial_variables)
-       
-   end
-  
-  (35).times do
-    x = arry[0]+arry[1]
-    arry[0]=arry[1]
-    break if x>=4000000
-    arry[1]=x
-      puts arry[1]
-  end
-  =end
+=end
+ 
    
    
