b02c7b9dce50f81a6a0afa56dae6212f3aa6e1cf
sebastianrodo <sebastian.rodriguez@koombea.com>
2020-11-09 13:51:32 -0500

    Update palindrome word exercise using return

--- Ruby/palindrome-word.rb
+++ Ruby/palindrome-word.rb
@@ -2,8 +2,6 @@ puts "Ingrese alguna palabra la cual quiera saber si es palindroma"
 
 x = gets.chomp.downcase
 
-if (x === x.to_s.reverse) 
-    puts "Si es una palabra palindroma"
-else
-    puts "No es una palabra palindroma"
-end
+return puts "Es un palindrome" if x === x.to_s.reverse
+
+puts "No es un palindrome"
