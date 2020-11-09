58da6e9a8196168277b00cf5d0ea690bfe95c44c
sebastianrodo <sebastian.rodriguez@koombea.com>
2020-11-09 14:18:15 -0500

    Update sum square difference exercise

--- Ruby/Multiplos-3-5.rb
+++
@@ -1,4 +0,0 @@
-puts "Los multiplos de 3 y 5 son :"
-puts (1...1000).to_a.select{|x| x if (x % 3 == 0 || x % 5 == 0) }
-puts "La suma de los multiplos de 3 y 5 es :"
-puts (1...1000).to_a.select{|x| x if (x % 3 == 0 || x % 5 == 0) }.sum
--- Ruby/sum_square_difference.rb
+++ Ruby/sum_square_difference.rb
@@ -1,9 +1,9 @@
-c = 0
-def difference(c)
-  a = (1..100).map { |n| n ** 2 }.sum
-  b = (1..100).sum ** 2
-  c = b - a
+
+def difference
+  sum_of_squares = (1..100).map { |n| n ** 2 }.sum
+  square_of_sum = (1..100).sum ** 2
+  sum_of_squares - square_of_sum
 end
 
-puts "The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is :"
-puts difference(c)
+puts "The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is : #{difference}"
+
