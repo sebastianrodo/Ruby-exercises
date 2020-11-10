951a5999c1498a79cf47c0fa2e3d0e66f389cd1f
sebastianrodo <73562398+sebastianrodo@users.noreply.github.com>
2020-11-10 11:03:21 -0500

    Add Factorial of a number (#3)
    * Add multiples 3 and 5 exercise
    
    * Add Fibonacci Numbers exercise
    
    * Add Factorial of a number exercise
    
    * Add Factorial with Enumerables, exercise
    
    * update factorial of a number exercise
    
    * Add a blank extra line at the end of this exercise
    
    * Update Factorial of a number exercise

---
+++ Ruby/Factorial.rb
@@ -0,0 +1,8 @@
+puts "Enter the value for which you want to calculate the factorial!"
+number = gets.chomp.to_i
+def factorial(number)
+  number == 0 ? "1" : (1..number).reduce(:*).to_s
+end
+
+puts "The factorial value is : " + factorial(number)
+
