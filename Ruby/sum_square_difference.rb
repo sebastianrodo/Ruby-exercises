1d85226ecc5a0158d6106e7af9f9ebf26e88c8af
sebastianrodo <sebastian.rodriguez@koombea.com>
2020-11-10 11:47:35 -0500

    Update Sum square difference exercise

--- Ruby/sum_square_difference.rb
+++ Ruby/sum_square_difference.rb
@@ -1,9 +1,8 @@
-
-def difference
+def sum_square_difference
   sum_of_squares = (1..100).map { |n| n ** 2 }.sum
-  square_of_sum = (1..100).sum ** 2
-  sum_of_squares - square_of_sum
+  square_of_sums = (1..100).sum ** 2
+  square_of_sums - sum_of_squares 
 end
 
-puts "The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is : #{difference}"
+puts "The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is : #{sum_square_difference}"
 
