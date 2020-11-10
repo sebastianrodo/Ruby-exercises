61573c78b6fdf36e413c342d38efcaba1e2ee4b1
sebastianrodo <sebastian.rodriguez@koombea.com>
2020-11-10 09:54:09 -0500

    Update Fibonacci sequence exercise

--- ruby/Fibonacci.rb
+++ ruby/Fibonacci.rb
@@ -1,53 +1,12 @@
+MAX_VALUE = 4000000
+def fibonacci2
+  (1..MAX_VALUE).each_with_object([1,2]) do |num, acum|
+    final_value = acum[acum.size - 1] + acum[acum.size - 2]
+    return acum if final_value > MAX_VALUE 
 
-  def fibonacci2
-    @MAX_VALUE = 4000000
-    
-
-    (1..@MAX_VALUE).each_with_object([1,2]) do |num, acum|
-      final_value = acum[acum.size - 1] + acum[acum.size - 2]
-      return acum if final_value > @MAX_VALUE 
-
-      acum << final_value
-    end
+    acum << final_value
   end
+end
 
-  puts "The Fibonacci sequence is: #{fibonacci2}"
+puts "The Fibonacci sequence is: #{fibonacci2}"
 
-=begin
-  puts "The Fibonacci sequence is : "
-  initial_variables = [0, 1]
-   def(initial_variables)
-       
-   end
-  
-  (35).times do
-    x = arry[0]+arry[1]
-    arry[0]=arry[1]
-    break if x>=4000000
-    arry[1]=x
-      puts arry[1]
-  end
-=end
-
-=begin
-
-
-def fibonacci(max_value)
-    puts "The Fibonacci sequence is : "
-  
-    initial_value1 = 1
-    initial_value2 = 1
-   
-    while initial_value2 <= max_value
-      yield initial_value2
-  
-      final_value = initial_value1 + initial_value2
-      initial_value1 = initial_value2
-      initial_value2 = final_value 
-    end
-   
-  end
-   
-  fibonacci(4000000) {|x| puts x }
-=end
- 
