e7fa4aa5f4117799c9b17a9cf8a7cb0d3c619818
sebastianrodo <sebastian.rodriguez@koombea.com>
2020-11-11 09:59:44 -0500

    Update 10001st prime number exercise

--- Ruby/10001st-number.rb
+++ Ruby/10001st-number.rb
@@ -1,8 +1,10 @@
 require 'prime'
 
-def prime_number
-  Prime.first (10001)
+PRIME_NUMBER_POSITION = 10001
+
+def prime_number_max
+  Prime.first(PRIME_NUMBER_POSITION).max
 end
 
-puts "The 10001 prime number is : #{prime_number.max}"
+puts "The 10001 prime number is : #{prime_number_max}"
 
